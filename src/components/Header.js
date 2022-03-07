c09d2141037bce22815cb31d0c934896fe393b6d
Kharko7 <hanley77@ukr.net>
2022-03-08 00:29:39 +0200

    first_commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header className="header">
+			<a className="header__logo" href="#">
+				<img src="../img/logo.svg" />
+			</a>
+
+		</header>
+	)
+}
+
+export default Header; 
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+	return (
+		<nav className="nav">
+			<ul className="nav__list">
+				<li><a href="#" className="nav__link">Profile</a></li>
+				<li><a href="#" className="nav__link">Message</a></li>
+				<li><a href="#" className="nav__link">News</a></li>
+				<li><a href="#" className="nav__link">Music</a></li>
+			</ul>
+
+		</nav>
+	)
+}
+
+export default NavBar; 
